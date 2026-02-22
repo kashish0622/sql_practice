43e67ff3cfc21f32efcb1623d1a2692e4b380426
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-22 19:25:39 +0530

    added constraints, foreign key

--- DDL_Commands/create_practice.sql
+++ DDL_Commands/create_practice.sql
@@ -1,10 +1,18 @@
 CREATE DATABASE company_db;
 USE company_db;
+CREATE TABLE departments(
+dept_id INT PRIMARY KEY,
+dept_name VARCHAR(50)
+);
+
 CREATE TABLE employees(
 employee_id INT PRIMARY KEY,
-employee_name VARCHAR(50),
-age INT,
+employee_name VARCHAR(50) NOT NULL,
+age INT CHECK(age > 18),
 salary INT,
-joining_date DATE
+dept_id INT,
+joining_date DATE,
+FOREIGN KEY (dept_id) references departments(dept_id)
 );
+
 
