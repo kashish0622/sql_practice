b5214b2712eed5e55a1a6bd1f3322ee382465208
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-22 21:26:06 +0530

    dropped index using drop command

---
+++ src/DDL_Commands/drop_practice.sql
@@ -0,0 +1,31 @@
+CREATE TABLE departments(
+dept_id INT PRIMARY KEY,
+dept_name VARCHAR(50)
+);
+
+CREATE TABLE employees(
+employee_id INT PRIMARY KEY,
+employee_name VARCHAR(50) NOT NULL,
+age INT CHECK(age > 18),
+salary INT,
+dept_id INT,
+joining_date DATE,
+FOREIGN KEY (dept_id) references departments(dept_id)
+);
+
+CREATE INDEX idx_joining_date
+ON employees(joining_date)
+
+ALTER TABLE employees
+RENAME TO STAFF;
+ALTER TABLE STAFF
+ADD gender VARCHAR(10);
+ALTER TABLE departments
+MODIFY dept_name VARCHAR(30);
+ALTER TABLE STAFF
+DROP COLUMN gender;
+ALTER TABLE STAFF
+MODIFY salary float8;
+
+ALTER TABLE STAFF
+DROP INDEX idx_joining_date;
