d0823fbbec82164d6078c1414056c80c2f660e99
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-23 11:43:41 +0530

    delete query added in the file

---
+++ src/DML_Commands/delete_practice.sql
@@ -0,0 +1,25 @@
+CREATE TABLE departments(
+dept_id INT PRIMARY KEY,
+dept_name VARCHAR(50)
+);
+
+INSERT INTO departments (
+dept_id, dept_name)
+VALUES ('111', 'CSE'),
+('112', 'AI-ML'),
+('113', 'DATA-SCIENCE'),
+('114', 'IOT');
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE departments
+SET dept_name = 'CSE-plain'
+WHERE dept_id = '111';
+
+DELETE FROM departments
+WHERE dept_name = 'IOT';
+
+DELETE FROM departments
+WHERE dept_id = 111;
+
+
