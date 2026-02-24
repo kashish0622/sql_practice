575e097722f3a0825d52ef7e6d0504dbe3cd5286
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-24 21:25:15 +0530

    added subqueries for the company database

---
+++ src/Subquery/subquery.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE company_database;
+USE company_database;
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department_id INT,
+    salary INT
+);
+
+INSERT INTO departments VALUES
+(1, 'HR'),
+(2, 'IT'),
+(3, 'Finance'),
+(4, 'Marketing');
+
+INSERT INTO employees VALUES
+(101, 'Rahul', 2, 60000),
+(102, 'Priya', 1, 40000),
+(103, 'Amit', 2, 80000),
+(104, 'Sneha', 3, 75000),
+(105, 'Karan', 4, 50000);
+
+SELECT emp_name, salary
+FROM employees
+WHERE salary > (
+select avg(salary)
+FROM employees
+);
+
+SELECT emp_name
+FROM employees
+WHERE department_id IN (
+SELECT department_id
+FROM departments
+WHERE department_name = 'IT');
+
+SELECT emp_name, emp_id, salary
+FROM employees
+WHERE salary = (
+SELECT avg(salary)
+FROM employees
+);
+
+SELECT emp_name, emp_id, department_id
+FROM employees
+WHERE department_id = (
+SELECT department_id
+FROM departments
+WHERE department_name = 'Marketing' );
+
+
+
+
+
+
+
