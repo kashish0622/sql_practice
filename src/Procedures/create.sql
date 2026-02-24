07e6bdc438477a1704a28558c938a84313ee34b0
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-24 19:41:50 +0530

    created a procedure with a parameter

--- src/Procedures/create.sql
+++ src/Procedures/create.sql
@@ -7,4 +7,19 @@ BEGIN
 SELECT * FROM student;
 END //
 DELIMITER ; 
+DROP PROCEDURE information;
 
+use studentinfo;
+SELECT * FROM student;
+
+DELIMITER //
+CREATE PROCEDURE information(
+IN s_ID INT,
+IN s_name VARCHAR(50)) 
+BEGIN
+SELECT * FROM student
+WHERE roll_no = s_ID
+AND name = p_name;
+END //
+DELIMITER ; 
+DROP PROCEDURE information;
