e882accc3aa3121a97419ae2c0efbb7a8874d387
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-25 00:00:47 +0530

    run the window function on the database

---
+++ src/window_functions/aggregatewindow.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE company_database;
+USE company_database;
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department_id INT,
+    salary INT
+);
+
+INSERT INTO departments VALUES
+(1, 'HR'),
+(2, 'IT'),
+(3, 'Finance'),
+(4, 'Marketing');
+
+INSERT INTO employees VALUES
+(101, 'Rahul', 2, 60000),
+(102, 'Priya', 1, 40000),
+(103, 'Amit', 2, 80000),
+(104, 'Sneha', 3, 75000),
+(105, 'Karan', 4, 50000);
+
+SELECT emp_id, emp_name, salary,
+AVG(salary) OVER (PARTITION BY department_id) AS aggregate_function
+FROM employees;
