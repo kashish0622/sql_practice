55352ab829b3f41e7973d5712b6c73fe93a502f9
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-24 19:54:57 +0530

    dropped the procedure created using drop command

---
+++ src/Procedures/drop.sql
@@ -0,0 +1,14 @@
+use studentinfo;
+SELECT * FROM student;
+
+DELIMITER //
+CREATE PROCEDURE information()
+BEGIN
+SELECT * FROM student;
+END //
+DELIMITER ;
+ 
+ALTER PROCEDURE information
+comment 'cannot alter bcoz mysql doesnt allow that';
+CALL information();
+DROP procedure information;
