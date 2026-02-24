fd6f019a01c8293180e62388c97707ca0094776f
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-24 19:53:25 +0530

    executed the procedure using call function in mysql

---
+++ src/Procedures/execute.sql
@@ -0,0 +1,13 @@
+use studentinfo;
+SELECT * FROM student;
+
+DELIMITER //
+CREATE PROCEDURE information()
+BEGIN
+SELECT * FROM student;
+END //
+DELIMITER ;
+ 
+ALTER PROCEDURE information
+comment 'cannot alter bcoz mysql doesnt allow that';
+CALL information();
