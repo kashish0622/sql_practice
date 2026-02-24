ffbc9230e02a0266788bb4817015a900ae1446d7
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-24 23:51:59 +0530

    dropped/deleted the view created

---
+++ src/Views/dropview.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE company_database;
+USE company_database;
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department_id INT,
+    salary INT
+);
+
+INSERT INTO departments VALUES
+(1, 'HR'),
+(2, 'IT'),
+(3, 'Finance'),
+(4, 'Marketing');
+
+INSERT INTO employees VALUES
+(101, 'Rahul', 2, 60000),
+(102, 'Priya', 1, 40000),
+(103, 'Amit', 2, 80000),
+(104, 'Sneha', 3, 75000),
+(105, 'Karan', 4, 50000);
+
+CREATE VIEW view1 as 
+SELECT emp_name, emp_id FROM employees;
+
+SELECT * FROM view1;
+DROP VIEW view1;
